3c8cb17d8a45f1510b1e15db5e7e190768b99290
jignesh119 <jignesh3503@gmail.com>
2024-08-25 00:42:32 +0530

    implented leave server,delete server modal, api endpoints

--- app/api/channels/[serverId]/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function POST(req: Request) {
-  try {
-  } catch (error) {
-    console.log("[SERVER_ID_POST]", error);
-    return new NextResponse("Internal Server Error", { status: 500 });
-  }
-}
