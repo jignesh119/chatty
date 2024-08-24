c2b59fea93a411f5de2c74e1d2f018fed42fee65
jignesh119 <jignesh3503@gmail.com>
2024-08-24 11:12:02 +0530

    bootstrapped app, setup clerk authentication

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div className="bg-red font-serif">hello chatty</div>;
+}
---
+++ app/test/page.tsx
@@ -0,0 +1,4 @@
+
+export default async function TestPage(props: {}) {
+  return <div>My TestPage </div>;
+}
